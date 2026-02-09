578588b42a2b3ef58250d50de3d9591021ee718e
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2026-02-09 14:00:38 +0100

    Update to remove unused entities.

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -6,39 +6,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 public class ShoppingCartTest {
-    @Mock
-    private ShoppingCart shoppingCart;
 
     @InjectMocks
     private ShoppingCart shoppingCartImpl;
     private final String testItemId = "testItemId";
     private final String testItemName = "testItemName";
-    private final String testItem = "testItem1";
     private final int testItemPrice = 0;
     private final int testItemQuantity = 0;
     private final String cartId = "cart1";
-    private final String shoppingId = "shopping1";
     private final Double discount = 0.0;
 
 
